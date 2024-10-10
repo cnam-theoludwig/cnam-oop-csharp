29aeeeac4d860cfce271e8db79171b13a5bb64eb
Théo LUDWIG <contact@theoludwig.fr>
2024-10-10 08:59:59 +0200

    feat(TP1): Player

---
+++ Ludwig_Theo_Tp1/Player.cs
@@ -0,0 +1,51 @@
+public class Player
+{
+    private string FirstName { get; }
+    private string LastName { get; }
+    public string Alias { get; set; }
+    public string Name
+    {
+        get
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+    }
+
+    public Player(string firstName, string lastName, string alias)
+    {
+        this.FirstName = Player.FormatName(firstName);
+        this.LastName = Player.FormatName(lastName);
+        this.Alias = alias;
+    }
+
+    private static string FormatName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            return string.Empty;
+        }
+        return char.ToUpper(name[0]) + name.Substring(1).ToLower();
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Alias} ({this.Name})";
+    }
+
+    public override bool Equals(object? other)
+    {
+        Player? otherPlayer = other as Player;
+        return otherPlayer is not null && Equals(otherPlayer);
+    }
+
+    public bool Equals(Player other)
+    {
+        return other != null &&
+               Alias == other.Alias;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Alias);
+    }
+}
