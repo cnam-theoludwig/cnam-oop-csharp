263416176815e1fd4da17ff01ac2182bbde885c1
Théo LUDWIG <contact@theoludwig.fr>
2024-10-10 09:09:19 +0200

    feat(TP1): SpaceInvaders

---
+++ Ludwig_Theo_Tp1/SpaceInvaders.cs
@@ -0,0 +1,26 @@
+public class SpaceInvaders
+{
+    private List<Player> players;
+
+    public SpaceInvaders()
+    {
+        this.players = new List<Player>();
+        this.Init();
+    }
+
+    private void Init()
+    {
+        this.players.Add(new Player("Emmet", "Brown", "EB"));
+        this.players.Add(new Player("Jeanne", "Calment", "JC"));
+        this.players.Add(new Player("Théo", "Ludwig", "Divlo"));
+    }
+
+    public static void Main(string[] args)
+    {
+        SpaceInvaders game = new SpaceInvaders();
+        foreach (Player player in game.players)
+        {
+            Console.WriteLine(player.ToString());
+        }
+    }
+}
