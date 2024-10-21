19cb900cfadc12d941b6ad43bd1eef838605561d
Théo LUDWIG <contact@theoludwig.fr>
2024-10-21 14:25:16 +0200

    feat(TP2): add interfaces

---
+++ Ludwig_Theo_Tp2/IPlayer.cs
@@ -0,0 +1,8 @@
+﻿namespace Ludwig_Theo_Tp2;
+
+public interface IPlayer
+{
+    Spaceship BattleShip { get; set; }
+    string Name { get; }
+    string Alias { get; }
+}
---
+++ Ludwig_Theo_Tp2/IWeapon.cs
@@ -0,0 +1,14 @@
+﻿namespace Ludwig_Theo_Tp2;
+
+public interface IWeapon
+{
+    string Name { get; set; }
+    EWeaponType Type { get; set; }
+    double MinDamage { get; set; }
+    double MaxDamage { get; set; }
+    double AverageDamage { get; }
+    double ReloadTime { get; set; }
+    double TimeBeforReload { get; set; }
+    bool IsReload { get; }
+    double Shoot();
+}
